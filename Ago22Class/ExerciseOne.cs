927270b8004bf6cce5fddf8a44b23c48c8ef39fa
rnbotinac <142951954+rnbotinac@users.noreply.github.com>
2023-08-22 20:46:02 -0500

    Ejercicio1

---
+++ Ago22Class/ExerciseOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ago22Class
+{
+    public class ExerciseOne
+    {
+        public static void ExecuteExercise() { 
+            //
+        
+        }
+    }
+}
