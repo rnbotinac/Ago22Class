f5f04de8a2efae0c041048e41dac675843d38fa4
rnbotinac <142951954+rnbotinac@users.noreply.github.com>
2023-08-22 20:35:56 -0500

    Config inicial
    Config inicial

---
+++ Ago22Class/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ago22Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
